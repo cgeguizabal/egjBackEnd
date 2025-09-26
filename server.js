985004ac94deadc640c6c48b14661ef4c80687f0
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-09-25 18:22:17 -0600

    Production

--- server.js
+++ server.js
@@ -46,4 +46,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}`));
 
 //Production enviroment
-// export default app;
+export default app;
